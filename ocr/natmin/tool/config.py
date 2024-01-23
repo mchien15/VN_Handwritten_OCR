caf3eb715fff12e514430f81c72604df32e1aedd
mchien15 <chien.lm204518@sis.hust.edu.vn>
2024-01-24 00:44:57 +0700

    init

---
+++ ocr/natmin/tool/config.py
@@ -0,0 +1,35 @@
+import yaml
+from natmin.tool.utils import download_config
+
+url_config = {
+        'vgg-transformer':'vgg-transformer.yml',
+        'efficient-transformer':'efficient-transformer.yml',
+        'base':'base.yml',
+        }
+
+class Cfg(dict):
+    def __init__(self, config_dict):
+        super(Cfg, self).__init__(**config_dict)
+        self.__dict__ = self
+
+    @staticmethod
+    def load_config_from_file(fname):
+        #base_config = download_config(url_config['base'])
+        base_config = {}
+        with open(fname, encoding='utf-8') as f:
+            config = yaml.safe_load(f)
+        base_config.update(config)
+
+        return Cfg(base_config)
+
+    @staticmethod
+    def load_config_from_name(name):
+        base_config = download_config(url_config['base'])
+        config = download_config(url_config[name])
+
+        base_config.update(config)
+        return Cfg(base_config)
+
+    def save(self, fname):
+        with open(fname, 'w') as outfile:
+            yaml.dump(dict(self), outfile, default_flow_style=False, allow_unicode=True)
